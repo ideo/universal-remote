677d4b2dc5c67880e03673a438650e3277c9fdc0
Joe Gambino <joe@gam.bingo>
2024-02-16 14:17:35 -0600

    actual code

---
+++ src/imdb_scraper.py
@@ -0,0 +1,20 @@
+from imdb import Cinemagoer, IMDbDataAccessError
+
+
+IMDB = Cinemagoer()
+
+
+def get_synopsis(movie_title):
+    responses = IMDB.search_movie(movie_title)
+    first_hit = responses[0]
+
+    try:
+        details = IMDB.get_movie(first_hit.getID())
+        plot_outline = details["plot outline"]
+        print(f"{first_hit} ✓")
+        return plot_outline
+
+    except (KeyError, IMDbDataAccessError):
+        return ""
+
+    
