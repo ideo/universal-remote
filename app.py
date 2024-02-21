2f4c1496f4d8034d1841213a2915c0edbd3f0671
Joe Gambino <joe@gam.bingo>
2024-02-21 14:51:50 -0600

    reduced dimensions searcher

--- app.py
+++ app.py
@@ -1,49 +1,12 @@
 import streamlit as st
-from scipy import spatial
-import numpy as np
 
 from app import logic as lg
 
 
-
-st.set_page_config(page_icon="🎤",
-                   page_title="Seinfeld Searcher",
-                   layout="centered",)
-
-st.title("Seinfeld")
-df = lg.load_data()
-episodes = df.index.tolist()
-
-TREE = spatial.KDTree(df.values)
-
-col1, _plus, col2 = st.columns([5,1,5])
-# , _equals, col3 = st.columns([5,2,5,2,5])
-
-with col1:
-    ep1 = st.selectbox("Select an Episode", options=episodes, key="one")
-
-with _plus:
-    st.markdown("#### +")
-
-with col2:
-    ep2 = st.selectbox("Select an Episode", options=episodes, key="two")
-
-# with _equals:
-#     st.markdown("#### =")
-
-# with col3:
-_, cntr, _ = st.columns([2,5,2])
-with cntr:
-    # _sum = df.loc[ep1].values + df.loc[ep2].values
-    v1, v2 = df.loc[ep1].values, df.loc[ep2].values
-    _mean = np.mean(np.array([v1, v2]), axis=0)
-    distances, indices = TREE.query(_mean, k=3)
-    episode_names = [df.iloc[ii].name for ii in indices]
-    episode_names = [ep for ep in episode_names if ep not in [ep1, ep2]]
-    st.write(episode_names[0])
-
-    # st.write(distances, indices)
-    # st.write(df.iloc[indices[0]].name)
+st.set_page_config(page_title="Movie Searcher", page_icon="🎬")
+st.title("Movie Searcher")
 
 
-    # st.write(df.iloc[ii].name)
+synopses, embeddings = lg.load_movie_summary_embeddings()
+embeddings = lg.reduce_dimensions(embeddings)
+st.write(embeddings)
--- app/logic.py
+++ app/logic.py
@@ -1,14 +1,51 @@
+import os
+
 import streamlit as st
 import pandas as pd
+from umap import UMAP
 
 from src.directories import DATA_DIR
 
 
-def load_data():
+def load_seinfeld_embeddings():
     filepath = DATA_DIR / "seinfeld_embedding_vectors.pkl"
     df = pd.read_pickle(filepath)
     df = df.dropna(axis=0)
     return df
 
 
-# def add_vectors(v1, v2):
+@st.cache_data
+def load_movie_summary_embeddings():
+    filepath = DATA_DIR / "movie_embeddings_openai.csv"
+    df = pd.read_csv(filepath)
+
+    # Reduce
+    df.set_index("Movie Title", inplace=True)
+    df.drop(columns=["Unnamed: 0.1", "Unnamed: 0", "Movie URL"], inplace=True)
+    df.dropna(axis=0, inplace=True)
+
+    synopses = df["Synopsis"].copy()
+    embeddings = df.drop(columns=["Synopsis"])
+    return synopses, embeddings
+
+
+def reduce_dimensions(embeddings_df, num_dimensions=20,
+                      n_neighbors=17, min_dist=0.3):
+    filename = f"movie_synopses-{num_dimensions}_dims.pkl"
+    filepath = DATA_DIR / "synopses_embeddings" / filename
+
+    if os.path.exists(filepath):
+        reduced = pd.read_pickle(filepath)
+
+    else:
+        embeddings = embeddings_df.values
+        model = UMAP(n_neighbors=n_neighbors, min_dist=min_dist,
+                     n_components=num_dimensions, random_state=42)
+        reduced = model.fit_transform(embeddings)
+        reduced = pd.DataFrame(index=embeddings_df.index, data=reduced)
+        reduced.to_pickle(filepath)
+
+    return reduced
+
+
+
