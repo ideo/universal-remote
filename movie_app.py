6527974b0d90f17a53273e5da717deab21858e08
Emily Mo <emilymo008@gmail.com>
2024-02-22 16:15:20 -0500

    movie averager app is slightly more functional

--- movie_app.py
+++ movie_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from sklearn.metrics.pairwise import cosine_similarity
 import dotenv
 from scipy import spatial
@@ -16,26 +16,15 @@ class Bot:
     def __init__(self, embeddings_df):
         self.bot = ChatOpenAI(model_name='gpt-4-turbo-preview')
         self.embeddings_df = embeddings_df[embeddings_df['dim1'].notna()]
-        self.messages = [SystemMessage(content =
-                          """ 
-                            Return a Python list of dictionaries. There should be one dictionary for each movie the user mentions. 
-                            Each dictionary should have the following key/value pairs:
-                            The value of the key "movie" should state a movie mentioned by the user, spelled exactly as the user did. 
-                            The value of all of the "weight" keys should add up to 1. 
-                            If the user asks for a simple average or "cross between" a list of movies, 
-                            the weight should be distributed evenly across all movies.
-                            Otherwise, if the user suggests that one movie should be weighted heavier or lighter, interpret their
-                            words and adjust the weights accordingly.
-                            For instance, if the user says "I want to watch a movie like Movie A but a little more like Movie B", 
-                            an appropriate weighting would be 0.8 for Movie A and 0.2 for Movie B.
-                            Do not format response, output it in plain text.
-                        """)]
+        self.messages = []
 
     def average_embeddings(self, dict_list):  # average two movies and return closest suggestion
         # right now we are assuming that everything is spelled the same way as in the database
         just_embeddings = self.embeddings_df.filter(regex='dim')
         titles = [item['movie'] for item in dict_list]
         rows = [just_embeddings[self.embeddings_df['Movie Title'] == item['movie']] for item in dict_list]
+        if len([row for row in rows if row.empty]) > 0:  # make sure at least one movie was identified in our data
+            raise Exception('less than 2 of the movies you mentioned were identified in our database!')
         weights = [item['weight'] for item in dict_list]
         # print(rows)
         # print(weights)
@@ -54,12 +43,41 @@ class Bot:
     def process_input(self, user_input = None):  # processes user's initial message
         if user_input is None:
             user_input = input('What would you like to watch today?')
+        self.messages.append(  # TODO: use ChatMessageHistory instead
+            SystemMessage(content=
+                          """ 
+                            Return a Python list of dictionaries. There should be one dictionary for each movie the user mentions. 
+                            Each dictionary should have the following key/value pairs:
+                            The value of the key "movie" should state a movie mentioned by the user, spelled exactly as the user did. 
+                            The value of all of the "weight" keys should add up to 1. 
+                            If the user asks for a simple average or "cross between" a list of movies, 
+                            the weight should be distributed evenly across all movies.
+                            Otherwise, if the user suggests that one movie should be weighted heavier or lighter, interpret their
+                            words and adjust the weights accordingly.
+                            For instance, if the user says "I want to watch a movie like Movie A but a little more like Movie B", 
+                            an appropriate weighting would be 0.8 for Movie A and 0.2 for Movie B.
+                            Do not format response, output it in plain text.
+                        """))
         self.messages.append(HumanMessage(content = user_input))
         response = self.bot.invoke(self.messages)
+        self.messages.append(response)
         print(response)
         return eval(response.content)  # TODO: need to do some spelling deviation correction here. can we use movie titles embeddings / search for this?
 
     # TODO: spit out the recs in plain language
+    def give_recs(self, recs):
+        self.messages.append(
+            SystemMessage("""
+                The input message is a Python list of movie titles. 
+                You are to recommend these movies in natural language based on their previous query.
+                Don't justify the recommendations, just offer them concisely.
+            """))
+        self.messages.append(HumanMessage(content = str(recs)))
+        response = self.bot.invoke(self.messages)
+        self.messages.append(response)
+        print(response)
+        return response.content
+
 
     # TODO: need chain to put everything together
 
@@ -70,14 +88,22 @@ embeddings = pd.read_csv('data/movie_embeddings_openai.csv')
 # init_response = testbot.process_input("I want a movie that's a cross between Finding Nemo and Rush Hour")  # use the godfather to test spelling deviation correction
 # print(testbot.average_embeddings(init_response))
 
-# I want a movie that's like Finding Nemo but a little more like Rush Hour  # this one puts more weight on rush hour
-# I want a movie that's like Finding Nemo but a little bit like Rush Hour
-# I want a movie that's like Finding Nemo but a tiny bit like Rush Hour
+"""
+I want a movie that's like Finding Nemo but a little more like Rush Hour  # this one puts more weight on rush hour
+I want a movie that's like Finding Nemo but a little bit like Rush Hour
+I want a movie that's like Finding Nemo but a tiny bit like Rush Hour
 
-# back to the future x goonies
-# zach: robocop x care bears
+Give me a cross between Robocop and Marley & Me
+Give me the love child of American Psycho and The Matrix
+
+back to the future x goonies
+zach: robocop x care bears
+"""
+
 
 testbot = Bot(embeddings)
 while True:
     init_response = testbot.process_input()
-    print(testbot.average_embeddings(init_response))
+    recs_list = testbot.average_embeddings(init_response)
+    # print(recs_list)
+    print(testbot.give_recs(recs_list))
